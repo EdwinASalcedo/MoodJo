cc2e12e46f5eee315a36b4a9a8468f1b0a56f4ce
EdwinASalcedo <92866752+EdwinASalcedo@users.noreply.github.com>
2025-12-26 15:46:40 -0500

    Initial Commit

---
+++ MoodJo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodJo
+//
+//  Created by Edwin Salcedo on 12/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodJo/MoodJoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodJoApp.swift
+//  MoodJo
+//
+//  Created by Edwin Salcedo on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodJoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
